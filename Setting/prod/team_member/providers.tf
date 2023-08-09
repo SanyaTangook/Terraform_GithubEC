8beaddcb211862d61096e2adb442c599424a044d
Sanya <champmc123@gmail.com>
2023-08-10 00:01:53 +0700

    prod team_member

--- Setting/prod/team_member/providers.tf
+++ Setting/prod/team_member/providers.tf
@@ -9,11 +9,6 @@ terraform {
   backend "azurerm" {}
 }
 
-# provider "github" {
-#   token = "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"
-#   owner = "uoio1"
-# }
-
 provider "github" {
   token = var.github_org_token
   owner = var.github_org_name

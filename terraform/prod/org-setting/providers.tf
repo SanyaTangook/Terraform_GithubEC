e74a03aabf48c708e13487877beedeac6c395eea
Sanya <champmc123@gmail.com>
2023-08-24 00:57:57 +0700

    add organization_security_manager.team

--- terraform/prod/org-setting/providers.tf
+++ terraform/prod/org-setting/providers.tf
@@ -1,20 +1,16 @@
 terraform {
-  # required_version = "1.5.5"
+  required_version = "1.5.5"
   required_providers {
     github = {
       source  = "integrations/github"
       version = "5.33.0"
     }
   }
-  # backend "azurerm" {}
+  backend "azurerm" {}
 }
-
-# provider "github" {
-#   token = var.github_org_token
-#   owner = var.github_org_name
-# }
 
 provider "github" {
-  token = "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"
-  owner = "uoio1"
+  token = var.github_org_token
+  owner = var.github_org_name
 }
+
